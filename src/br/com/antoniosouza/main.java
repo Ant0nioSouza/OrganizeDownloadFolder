e7fbc18fa8bfc790dfcfb388df4f57cd81fa9805
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2021-04-20 13:18:39 -0300

    Main file

---
+++ src/br/com/antoniosouza/main.java
@@ -0,0 +1,46 @@
+package br.com.antoniosouza;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        String[] extensions = new String[]{".pdf", ".txt", ".log", ".tgz", ".m4a", ".jar", ".jar", ".7z", ".tar.gz", ".mp4", ".rar", ".zip", ".jpg", ".png", ".jpeg", ".pptx", ".docx", ".java", ".dat", ".exe", ".mp3", ".htm", ".tflite"}; // File extensions array
+
+        String path = "C:\\Users\\Antonio\\Downloads";
+        File files = new File(path);
+        File[] list = files.listFiles();
+        System.out.println(Arrays.toString(list));
+        for (File file : list) {
+            String fileName = file.getName();
+            for (String extension : extensions) {
+                File extensionDirectory = new File("C:\\Users\\Antonio\\Downloads\\" + extension);
+                File directoryToExtensionFile = new File(String.valueOf(extensionDirectory) + "\\" + fileName);
+                File otherDirectory = new File("C:\\Users\\Antonio\\Downloads\\others");
+                File directoryToOtherFile = new File(String.valueOf(otherDirectory) + "\\" + fileName);
+                if (fileName.endsWith(extension)) {
+                    if (extensionDirectory.isDirectory()) {
+                        file.renameTo(new File(String.valueOf(directoryToExtensionFile)));
+                        System.out.println(file + " moved to " + directoryToExtensionFile);
+                    } else {
+                        extensionDirectory.mkdir();
+                        file.renameTo(new File(String.valueOf(directoryToExtensionFile)));
+                    }
+                } else {
+                    System.out.println(fileName);
+                    if (otherDirectory.isDirectory()) {
+                        if (!file.isDirectory()) {
+                            file.renameTo(new File(String.valueOf(directoryToOtherFile)));
+                        }
+                    } else {
+                        if (!file.isDirectory()) {
+                            otherDirectory.mkdir();
+                            file.renameTo(new File(String.valueOf(directoryToOtherFile)));
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
