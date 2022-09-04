03562b1498abf9c761db3ea093e4955e422aa12c
Antonio Souza <antoniosouza.dev@gmail.com>
2022-09-04 18:28:07 -0300

    feat(cpp): moving files to created folders

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -21,20 +21,39 @@ int createDir(const char *a) {
 
 int main() {
 	string currentPath = filesystem::current_path();
-
+	cout << " Current path: " << currentPath << endl;
 	for (const auto & entry : filesystem::directory_iterator(currentPath)) {
 		string filename = entry.path();
 		stringstream test(filename);
+		stringstream testT(filename);
 		string segment;
+		string segmentT;
 		vector<string> segList;
+		vector<string> lastList;
 		while (getline(test, segment, '.')) {
 			segList.push_back(segment);
 		}
+		while (getline(testT, segmentT, '/')) {
+			lastList.push_back(segmentT);
+		}
+
+		
+		stringstream folderTest(currentPath);
+		string folderSegment;
+		vector<string> folderList;
+		while (getline(folderTest, folderSegment, '/')) {
+			folderList.push_back(folderSegment);
+		}
+
 		if (segList.size() == 1)
 			continue;
 		string a = segList.back();
 		const char *extension = a.c_str();
 		createDir(extension);
-		cout << "FILE PATH: " << filename << endl;
+
+		//cout << "FILE PATH: " << filename << endl;
+		//cout << "Last path: " << currentPath + "/" + extension + "/" + lastList.back() << endl;
+		filesystem::rename(filename, currentPath + "/" + extension + "/" + lastList.back());
 	}
+	cout << "FINISHED WORK" << endl;
 }
